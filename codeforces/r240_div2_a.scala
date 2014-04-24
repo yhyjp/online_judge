e3cda5bd29021315dbe930f44bb802260d540ef4
yhyjp <yhyhyjp@gmail.com>
2014-04-24 20:17:25 +0900

    Mashmokh and Lights

---
+++ codeforces/r240_div2_a.scala
@@ -0,0 +1,14 @@
+object A extends App {
+  val sc = new java.util.Scanner(System.in);
+  val n, m = sc.nextInt();
+  var a = Array.fill(n)(-1)
+  val b = Array.fill(m)(sc.nextInt()).map(_ - 1)
+  for (t <- b) {
+    for (i <- t until n if (a(i) == -1)) a(i) = t
+    
+    // same-time
+    // val x = (t until n)
+    // x.filter(i => a(i) == -1).foreach(a(_) = t)
+  }
+  println(a.map(_ + 1).mkString(" "))
+}
