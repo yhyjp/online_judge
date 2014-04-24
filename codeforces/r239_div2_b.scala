24b6a8f220e52049401f651fdef87fd33f99e896
yhyjp <yhyhyjp@gmail.com>
2014-04-24 11:24:52 +0900

    Garland

---
+++ codeforces/r239_div2_b.scala
@@ -0,0 +1,20 @@
+object B extends App {
+  val a, b = readLine();
+  val ct = (s: String, c: Char) => { s.count(_ == c) }
+  val ctm = (l: String) => { l map (ct(l, _: Char)) }
+  val am = a.zip(ctm(a)).toMap
+  val bm = b.zip(ctm(b)).toMap
+  var impossible = false;
+  var sum = 0;
+  for ((k, n) <- bm)
+  {
+    if (am.contains(k) == false) {
+      impossible = true;
+    }
+    else {
+      sum += n min am(k)
+    }
+  }
+  if (impossible) sum = -1;
+  println(sum);
+}
