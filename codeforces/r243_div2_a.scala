ae590ef605913d6b555162a8ea5e5c61bf5fb554
yhyjp <yhyhyjp@gmail.com>
2014-04-29 20:38:25 +0900

    Sereja and Mugs

---
+++ codeforces/r243_div2_a.scala
@@ -0,0 +1,6 @@
+object A extends App {
+  val sc = new java.util.Scanner(System.in)
+  val n, s = sc.nextInt()
+  val t = Array.fill(n)(sc.nextInt()).sortWith(_ > _)
+  println(if (t.tail.sum <= s) "YES" else "NO")
+}
