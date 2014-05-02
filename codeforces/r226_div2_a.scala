75b50bac2ffa5473e95ed54226c4c0e58730d6c7
yhyjp <yhyhyjp@gmail.com>
2014-05-02 10:41:47 +0900

    Bear and Raspberry

---
+++ codeforces/r226_div2_a.scala
@@ -0,0 +1,10 @@
+object A extends App {
+  val sc = new java.util.Scanner(System.in)
+  val n, c = sc.nextInt()
+  val x = Array.fill(n)(sc.nextInt())
+  var ans = 0
+  for (i <- 0 until n-1) {
+    ans = ans.max(x(i)-x(i+1)-c)
+  }
+  println(ans)
+}
