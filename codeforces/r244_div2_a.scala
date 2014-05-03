222bdee4a00973e42fe65ad7c8f5849eb7cf8745
yhyjp <yhyhyjp@gmail.com>
2014-05-04 01:18:01 +0900

    Police Recruits

---
+++ codeforces/r244_div2_a.scala
@@ -0,0 +1,16 @@
+object A extends App {
+  val sc = new java.util.Scanner(System.in)
+  val n = sc.nextInt()
+  val ev = Array.fill(n)(sc.nextInt())
+  var unrated, hired = 0;
+  for (t <- ev) {
+    if (t == -1) {
+      if (hired == 0) unrated += 1
+      else hired -= 1
+    }
+    else {
+      hired += t
+    }
+  }
+  println(unrated);
+}
